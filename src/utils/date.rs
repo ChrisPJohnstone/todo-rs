5628aaa35f1270561bd9c3bdd2be64ef43b5dad0
Christopher Johnstone <chrisj97@proton.me>
2025-04-23 14:01:35 +0100

    Started parsing strings

--- src/utils/date.rs
+++ src/utils/date.rs
@@ -1,4 +1,13 @@
-use chrono::{DateTime, Local, TimeDelta, Timelike};
+use chrono::{DateTime, Local, NaiveDateTime, TimeDelta, Timelike};
+use chrono::offset::TimeZone;
+// use regex::Regex;
+
+// const YEAR_REGEX: &str = r"\d{4}";
+// const MONTH_REGEX: &str = r"(0\d|1[0-2])";
+// const DAY_REGEX: &str = r"([0-2]\d|3[0-1])";
+// const HOUR_REGEX: &str = r"([0-1]\d|2[0-3])";
+// const MINUTE_REGEX: &str = r"([0-5]\d)";
+// const SECOND_REGEX: &str = r"([0-5]\d)";
 
 fn now() -> DateTime<Local> {
     Local::now()
@@ -17,10 +26,25 @@ fn tomorrow() -> DateTime<Local> {
         + TimeDelta::days(1)
 }
 
+fn parse(string: &str) -> DateTime<Local> {
+    println!("Parsing date: `{}`", string);
+    let utc = NaiveDateTime::parse_from_str(string, "%Y-%m-%d %H:%M:%S");
+    println!("Parsed date: {:?}", utc);
+
+    Local.from_local_datetime(&utc.unwrap())
+        .unwrap()
+        .with_second(0).unwrap()
+        .with_nanosecond(0).unwrap()
+}
+
 pub fn parse_date(due: &str) -> DateTime<Local> {
-    match due {
-        "later" => later(),
-        "tomorrow" => tomorrow(),
-        _ => panic!("Invalid date format"),
+    if due == "later" {
+        return later();
     }
+    if due == "tomorrow" {
+        return tomorrow();
+    }
+    // TODO: Add regex stuff to guess date format and parse
+    return parse(due);
+    // panic!("Invalid date format");
 }
